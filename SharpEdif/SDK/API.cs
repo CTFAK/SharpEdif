30214cf3921711df5881103e40678b65de037a89
1987kostya <ivaninvideorec@gmail.com>
2023-05-19 23:30:00 +0600

    Revert previous commit, because my memory management system is a little bit broken

--- SharpEdif/SDK/API.cs
+++ SharpEdif/SDK/API.cs
@@ -249,11 +249,11 @@ namespace SharpEdif
             Utils.Log("GetObjInfos got called");
             //TODO We might need this one in future, idk
 #if EDITTIME
-            Utils.CopyStringToMemoryA(SDK.extName,ObjName,255);
-            Utils.CopyStringToMemoryA(SDK.extAuthor,ObjAuthor,255);
-            Utils.CopyStringToMemoryA(SDK.extCopyright,ObjCopyright,255);
-            Utils.CopyStringToMemoryA(SDK.extComment,ObjComment,1024);
-            Utils.CopyStringToMemoryA(SDK.extHttp,ObjHttp,255);
+            //Utils.CopyStringToMemoryA(SDK.extName,ObjName,255);
+            //Utils.CopyStringToMemoryA(SDK.extAuthor,ObjAuthor,255);
+            //Utils.CopyStringToMemoryA(SDK.extCopyright,ObjCopyright,255);
+            //Utils.CopyStringToMemoryA(SDK.extComment,ObjComment,1024);
+            //Utils.CopyStringToMemoryA(SDK.extHttp,ObjHttp,255);
 #endif
             
             
